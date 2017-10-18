43b38fbddeb34cff7b2df32157f41665d53c305a
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-10-18 09:05:10 -0700

    Set delegate for 2 textFields; resignFirstResponder

--- Scores-Revisited/ViewController.swift
+++ Scores-Revisited/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: Outlets
+    @IBOutlet weak var giantsScoreLabel: UILabel!
+    @IBOutlet weak var visitorScoreLabel: UILabel!
+    @IBOutlet weak var winningTeamLabel: UILabel!
+    @IBOutlet weak var giantsScoreTextField: UITextField!
+    @IBOutlet weak var visitorScoreTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set the View Controller as the delegate
+        giantsScoreTextField.delegate = self
+        visitorScoreTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: IBActions
+    @IBAction func whoWon(_ sender: UIButton) {
+        let giantsScore: Int = Int(giantsScoreLabel.text!)!
+        let visitorScore: Int = Int(visitorScoreLabel.text!)!
+        
+        if giantsScore < visitorScore {
+            winningTeamLabel.text = "Visitor"
+        } else if giantsScore > visitorScore {
+            winningTeamLabel.text = "Giants"
+        } else {
+            winningTeamLabel.text = "Tie"
+        }
+    }
+    
+    // MARK: Methods
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("textFieldShouldReturn")
+        giantsScoreTextField.resignFirstResponder()
+        visitorScoreTextField.resignFirstResponder()
+        return true
+    }
 
 }
 
